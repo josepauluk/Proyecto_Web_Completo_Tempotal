fb8428267bea91b6066c87176f1d814a3527fab4
Jose Pauluk <josepauluk@gmail.com>
2022-12-15 22:30:24 -0300

    formulario envio de mails funcionando

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -129,11 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
-#BASE_DIR / 'db.sqlite3'
+# BASE_DIR / 'db.sqlite3'
 
-EMAIL_BACKEND="django.core.mail.backend.smtp.EmailBackend"
+# Configuración de email
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER= 'josepauluk@gmail.com'
-EMAIL_HOST_PASSWORD='Tobias2507741'
+EMAIL_HOST_PASSWORD='mfkuaxxvqbyqwhjl'
