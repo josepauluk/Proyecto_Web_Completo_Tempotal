a017631dda78b7777bd3623559971dd4a1eb6dc7
Jose Pauluk <josepauluk@gmail.com>
2022-12-17 18:25:10 -0300

    Finalizado LOGUIN END

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as mensajes_de_error
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,12 @@ EMAIL_HOST_USER= 'josepauluk@gmail.com'
 EMAIL_HOST_PASSWORD='mfkuaxxvqbyqwhjl'
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+MESSAGE_TAGS={
+
+    mensajes_de_error.DEBUG: 'debug',
+    mensajes_de_error.INFO: 'info',
+    mensajes_de_error.SUCCESS: 'success',
+    mensajes_de_error.WARNING: 'warning',
+    mensajes_de_error.ERROR: 'danger',
+}
--- ProyectoWeb/login/urls.py
+++ ProyectoWeb/login/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import  VistaLogin, cerrar_sesion
+from .views import  VistaLogin, cerrar_sesion, logear
 
 
 urlpatterns = [    
         
     path('', VistaLogin.as_view(), name='Login'),    
 
-    path('', cerrar_sesion, name='cerrar_sesion'),
+    path('cerrar_sesion', cerrar_sesion, name='cerrar_sesion'),
+
+    path('logear', logear, name='logear'),
 ]
 
 
