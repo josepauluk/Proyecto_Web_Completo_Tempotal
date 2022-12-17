b8bc30d2be746f130b8e0c201a09d8d51478b33b
Jose Pauluk <josepauluk@gmail.com>
2022-12-17 16:04:03 -0300

    commit igua al anterior video 63

--- ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoWeb/ProyectoWeb/settings.py
@@ -140,11 +140,6 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER= 'josepauluk@gmail.com'
-<<<<<<< HEAD
-EMAIL_HOST_PASSWORD='*********'
-
+EMAIL_HOST_PASSWORD='mfkuaxxvqbyqwhjl'
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
-=======
-EMAIL_HOST_PASSWORD='mfkuaxxvqbyqwhjl'
->>>>>>> fb8428267bea91b6066c87176f1d814a3527fab4
